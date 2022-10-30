7691cd422eaf08ee9ba02cc704e3822604f90727
Akhil987000 <102216458+Akhil987000@users.noreply.github.com>
2022-10-30 14:13:08 +0530

    a,b=map(int,input().split()) x=list(map(int,input().split())) y=list(map(int,input().split())) c=[] for i in x:     if(i in y):         if(i in c):             continue         c.append(i) print(len(c))

---
+++ Count_elements_I.py
@@ -0,0 +1,10 @@
+a,b=map(int,input().split())
+x=list(map(int,input().split()))
+y=list(map(int,input().split()))
+c=[]
+for i in x:
+    if(i in y):
+        if(i in c):
+            continue
+        c.append(i)
+print(len(c))        
