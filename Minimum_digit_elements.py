ffef46be6ef2d4b30a4247564c6ef4029a0c21e3
Akhil987000 <102216458+Akhil987000@users.noreply.github.com>
2022-10-30 14:50:59 +0530

    a=int(input()) x=list(map(int,input().split())) c=[] for i in x:     if(i<0):         i=i*-1     z=list(str(i))     k=len(z)     c.append(k)     r=min(c) print(c.count(r))

---
+++ Minimum_digit_elements.py
@@ -0,0 +1,11 @@
+a=int(input())
+x=list(map(int,input().split()))
+c=[]
+for i in x:
+    if(i<0):
+        i=i*-1
+    z=list(str(i))
+    k=len(z)
+    c.append(k)
+    r=min(c)
+print(c.count(r))
