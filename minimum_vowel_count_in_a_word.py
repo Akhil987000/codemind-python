8b990aa556e1020b340b759fb20af5c363590629
Akhil987000 <102216458+Akhil987000@users.noreply.github.com>
2022-11-01 14:41:32 +0530

    a=input() s=a.split() def vowel(a):     c=0     for i in a:         if(i in "aeiou"):             c+=1     return c d=[] for j in s:     a=vowel(j)     d.append(a) print(min(d))

---
+++ minimum_vowel_count_in_a_word.py
@@ -0,0 +1,14 @@
+a=input()
+s=a.split()
+def vowel(a):
+    c=0
+    for i in a:
+        if(i in "aeiou"):
+            c+=1
+    return c
+d=[]
+for j in s:
+    a=vowel(j)
+    d.append(a)
+print(min(d))
+   
