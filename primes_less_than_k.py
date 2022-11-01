216139d2c0f637ccdf27855818b788b89de09fdd
Akhil987000 <102216458+Akhil987000@users.noreply.github.com>
2022-11-01 14:04:46 +0530

    n=int(input()) a=list(map(int,input().split())) l=int(input()) def primes(n):     if(n<=1):         return False     for i in range(2,n):         if(n%i==0):             return False     return True c=[] for j in a:     if(primes(j) and j<=l):         c.append(j) print(len(c))

---
+++ primes_less_than_k.py
@@ -0,0 +1,15 @@
+n=int(input())
+a=list(map(int,input().split()))
+l=int(input())
+def primes(n):
+    if(n<=1):
+        return False
+    for i in range(2,n):
+        if(n%i==0):
+            return False
+    return True
+c=[]
+for j in a:
+    if(primes(j) and j<=l):
+        c.append(j)
+print(len(c))        
