c7b80d25907de2a4f865002141d0f559f20d6afb
Akhil987000 <102216458+Akhil987000@users.noreply.github.com>
2022-11-02 14:56:00 +0530

    a=input() b=input() a=a.lower() b=b.lower() a=a.split() b=b.split() a=''.join(a) b=''.join(b) a=list(a) b=list(b) z=set(a).intersection(set(b)) z=list(z) z.sort() z=''.join(z) if(len(z)==0):     print(-1) else:     print(z)

---
+++ common_characters.py
@@ -0,0 +1,18 @@
+a=input()
+b=input()
+a=a.lower()
+b=b.lower()
+a=a.split()
+b=b.split()
+a=''.join(a)
+b=''.join(b)
+a=list(a)
+b=list(b)
+z=set(a).intersection(set(b))
+z=list(z)
+z.sort()
+z=''.join(z)
+if(len(z)==0):
+    print(-1)
+else:
+    print(z)
